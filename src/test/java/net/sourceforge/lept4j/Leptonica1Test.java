43934ea0265c820dde735d7e2d59ecc1c4ed7d73
roflox <wow.brog@gmail.com>
2022-02-15 09:58:39 +0100

    newer leptonica and darwin arm64 library

--- src/test/java/net/sourceforge/lept4j/Leptonica1Test.java
+++ src/test/java/net/sourceforge/lept4j/Leptonica1Test.java
@@ -39747,7 +39747,7 @@ public class Leptonica1Test {
     @Test
     public void testGetLeptonicaVersion() {
         System.out.println("getLeptonicaVersion");
-        String expResult = "leptonica-1.82.0";
+        String expResult = "leptonica-1.83.0";
         Pointer result = Leptonica1.getLeptonicaVersion();
         assertTrue(result.getString(0).startsWith(expResult));
     }
